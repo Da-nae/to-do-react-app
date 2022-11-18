88f8ce79d6613f4b4e70cd73d562d08ea9dec796
Da-nae <grosjean.danae@gmail.com>
2022-11-18 14:19:56 +0100

    added the list

--- src/components/entries.js
+++ src/components/entries.js
@@ -3,18 +3,20 @@ import Checkboxes from './checkboxes.js';
 function Entries() {
     return (
         <section className="toDoEntries">
-            <div>
-                <Checkboxes />
-                <p className="toDoSingleEntries">Style this page</p>
-            </div>
-            <div>
-                <Checkboxes />
-                <p className="toDoSingleEntries">Learn React</p>
-            </div>
-            <div>
-                <Checkboxes />
-                <p className="toDoSingleEntries">Be cool</p>
-            </div>
+            <ul>
+                <li>
+                    <Checkboxes />
+                    <p className="toDoSingleEntries">Style this page</p>
+                </li>
+                <li>
+                    <Checkboxes />
+                    <p className="toDoSingleEntries">Learn React</p>
+                </li>
+                <li>
+                    <Checkboxes />
+                    <p className="toDoSingleEntries">Be cool</p>
+                </li>
+            </ul>
         </section>
     )
 }
