170cb29d80a17eb1e5ac46d1799756077efbba19
Da-nae <grosjean.danae@gmail.com>
2022-11-18 14:26:34 +0100

    add the first map

--- src/components/entries.js
+++ src/components/entries.js
@@ -1,21 +1,17 @@
 import Checkboxes from './checkboxes.js';
 
 function Entries() {
+    const toDoEntries = ['Style this page','Learn React','Be cool'];
+
     return (
         <section className="toDoEntries">
             <ul>
-                <li>
-                    <Checkboxes />
-                    <p className="toDoSingleEntries">Style this page</p>
-                </li>
-                <li>
-                    <Checkboxes />
-                    <p className="toDoSingleEntries">Learn React</p>
-                </li>
-                <li>
-                    <Checkboxes />
-                    <p className="toDoSingleEntries">Be cool</p>
-                </li>
+                {toDoEntries.map((entry) => (
+                    <li>
+                        <Checkboxes />
+                        <p className="toDoSingleEntries">{entry}</p>
+                    </li>
+                ))}
             </ul>
         </section>
     )
