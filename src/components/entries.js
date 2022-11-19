415f61e5f58875c4fd70e4df5c7cbe0404c297cc
Da-nae <grosjean.danae@gmail.com>
2022-11-19 13:31:24 +0100

    add state

--- src/components/entries.js
+++ src/components/entries.js
@@ -1,12 +1,22 @@
 import Checkboxes from './checkboxes.js';
+import React, { useState } from "react";
 
 function Entries() {
-    const toDoEntries = ['Style this page','Learn React','Be cool'];
+    const firstEntries = ['Style this page','Learn React','Be cool'];
+    const [toDoEntries, doneEntries] = useState(firstEntries);
+        /* 
+        * useState returns two values inside an array
+        -> const myState = useState(...);
+        * The first value is always the current state - In our case, the current list of todos
+        -> const todos = myState[0];
+        * The second value is always a function that let's me update the current state
+        -> const setTodos = myState[1];
+        */
 
     return (
         <section className="toDoEntries">
             <ul>
-                {toDoEntries.map((entry) => (
+                {firstEntries.map((entry) => (
                     <li>
                         <Checkboxes />
                         <p className="toDoSingleEntries">{entry}</p>
