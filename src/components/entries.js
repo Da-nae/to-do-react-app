ac43991ddafb04e526136e21fe995f24d620ca9a
Da-nae <grosjean.danae@gmail.com>
2022-11-21 11:56:52 +0100

    adding the line-through

--- src/components/entries.js
+++ src/components/entries.js
@@ -7,22 +7,27 @@ function Entries() {
         {name :'Be cool', complete : true}
     ];
     const [entries, setEntries] = useState(initialEntries);
-        /* 
-        * useState returns two values inside an array
-        -> const myState = useState(...);
-        * The first value is always the current state - In our case, the current list of todos
-        -> const todos = myState[0];
-        * The second value is always a function that let's me update the current state
-        -> const setTodos = myState[1];
-        */
+    const handleChange = () => {
+        console.log(entries);
+        // setEntries(!entries);
+    };
+/* 
+    * useState returns two values inside an array
+    -> const myState = useState(...);
+    * The first value is always the current state - In our case, the current list of todos
+    -> const todos = myState[0];
+    * The second value is always a function that let's me update the current state
+    -> const setTodos = myState[1];
+*/
     return (
         <section className="toDoEntries">
             <ul>
                 {entries.map((entry, index) => (
-                    <li key={index}>
-                        <input type="checkbox" defaultChecked={entry.complete}/>
-                        <p className="toDoSingleEntries">{entry.name}</p>
-                    </li>
+
+                        <li key={index}>
+                            <input type="checkbox" defaultChecked={entry.complete} onChange={handleChange} id="opt-in"/>
+                            <label className="toDoSingleEntries" for="opt-in">{entry.name}</label>
+                        </li>
                 ))}
             </ul>
         </section>
